7442355015fb3676dcaad05cac77b7229f28aa1a
Laszlo Szoboszlai <szlaci83@gmail.com>
2017-06-24 22:01:19 +0100

    JDBC connection verified

---
+++ src/me/laszloszoboszlai/jdbc/VerifyJDBCConnection.java
@@ -0,0 +1,34 @@
+package me.laszloszoboszlai.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class VerifyJDBCConnection {
+
+	
+	
+	public static void main(String[] args) {
+		String host = "localhost";
+	    String port = "3306";
+		String dbName = "hb_student_tracker";
+        String options = "?useSSL=false";
+		String userName = "hbstudent";
+	    String passWord = "hbstudent";
+	
+		String JDBCUrl = "jdbc:mysql://" + host + ":" + port+ "/" + dbName + options;
+		
+		try {
+			System.out.println("Connecting to DB: " + dbName);
+			
+			Connection dbConn = DriverManager.getConnection(JDBCUrl, userName, passWord);
+			
+			System.out.println("Connected!");
+		}
+		catch (Exception ex){
+			System.out.println("Connection error!");
+		}
+	}
+
+}
